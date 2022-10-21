b1ba2b4a51350fae4babcf91c08a2ae6e97ccfb5
Aleksandar Djuran <djuran89@gmail.com>
2022-10-21 18:54:00 +0200

    upaloadimage

--- pages/proizvod/[category].js
+++ pages/proizvod/[category].js
@@ -6,17 +6,28 @@ import SingleProduct from "./../../components/product/product";
 import MobileHeader from "./../../components/header/mobileHeader";
 import Header from "./../../components/header/header";
 
-const Product = (props) => {
-	const router = useRouter();
-	const { category } = router.query;
-	const { products, isMobile, setQuantityValue, urlString, mainTitle } = props;
-	
-	const [prodRender, setprodRender] = React.useState([]);
+class Product extends React.Component {
+	constructor(props) {
+		super(props);
 
-	React.useEffect(() => {
-		const productName = decodeURI(props.urlString(window.location.hash).split("#")[1]);
+		this.state = { productName: "" };
+	}
+
+	componentDidMount() {
+		const productName = decodeURI(this.props.urlString(window.location.hash).split("#")[1]);
+		this.setState({ productName });
+	}
+
+	componentDidUpdate() {
+		console.log("Updated!");
+	}
+
+	render() {
+		const props = this.props;
+		const { category } = Router.router.query;
+		const { products, isMobile, setQuantityValue, urlString, mainTitle } = props;
+
 		const filterProducts = products.filter((el) => el.category === category.replace("-", " "));
-
 		const renderPorudcts = filterProducts.map((el, i) => (
 			<div id={`${urlString(el.name)}`} key={i}>
 				<SingleProduct
@@ -29,29 +40,29 @@ const Product = (props) => {
 				/>
 			</div>
 		));
-		setprodRender(renderPorudcts);
+		const productName = decodeURI(this.props.urlString(window.location.hash).split("#")[1]);
 		const link = document.createElement("a");
 		link.href = "#" + productName;
 		link.click();
-	}, [category]);
 
-	return (
-		<>
-			<Head>
-				<title>
-					{/* {mainTitle} - {productName.replaceAll("-", " ")} */}
-				</title>
-			</Head>
-			{isMobile ? (
-				<MobileHeader setQuantityValue={setQuantityValue} />
-			) : (
-				<Header products={products} setQuantityValue={setQuantityValue} />
-			)}
-			<div id="slider" className={`slider `}>
-				<div className="slides">{prodRender}</div>
-			</div>
-		</>
-	);
-};
+		return (
+			<>
+				<Head>
+					<title>
+						{mainTitle} - {productName.replaceAll("-", " ")}
+					</title>
+				</Head>
+				{isMobile ? (
+					<MobileHeader setQuantityValue={setQuantityValue} />
+				) : (
+					<Header products={products} setQuantityValue={setQuantityValue} />
+				)}
+				<div id="slider" className={`slider `}>
+					<div className="slides">{renderPorudcts}</div>
+				</div>
+			</>
+		);
+	}
+}
 
 export default Product;
